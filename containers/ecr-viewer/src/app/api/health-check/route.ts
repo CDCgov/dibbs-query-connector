58d9715382379475a7e7c27c57878d237615ad00
DanPaseltiner <dan@skylight.digital>
2024-10-11 12:07:13 -0400

    Remove all containers other than TEFCA Viewer

--- containers/ecr-viewer/src/app/api/health-check/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-/**
- * Health check for ECR Viwer
- * @returns Response with status OK.
- */
-export async function GET() {
-  return NextResponse.json({ status: "OK" }, { status: 200 });
-}
