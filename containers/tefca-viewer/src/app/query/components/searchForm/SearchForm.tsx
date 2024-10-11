49e7b1f59ed322b624e1c6b04debfba471e458d0
Rob Mitchell <robert.a.mitchell@skylight.digital>
2024-10-11 16:28:06 -0400

    removing default server

--- containers/tefca-viewer/src/app/query/components/searchForm/SearchForm.tsx
+++ containers/tefca-viewer/src/app/query/components/searchForm/SearchForm.tsx
@@ -67,7 +67,7 @@ const SearchForm: React.FC<SearchFormProps> = ({
   const [firstName, setFirstName] = useState<string>("");
   const [lastName, setLastName] = useState<string>("");
   const [fhirServer, setFhirServer] = useState<FHIR_SERVERS>(
-    "Public HAPI: Direct",
+    // "Public HAPI: Direct",
   );
   const [phone, setPhone] = useState<string>("");
   const [dob, setDOB] = useState<string>("");
@@ -250,6 +250,7 @@ const SearchForm: React.FC<SearchFormProps> = ({
                     name="fhir_server"
                     value={fhirServer}
                     defaultValue={""}
+                    // defaultValue={"Public HAPI: Direct"}
                     onChange={(event) => {
                       setFhirServer(event.target.value as FHIR_SERVERS);
                     }}
