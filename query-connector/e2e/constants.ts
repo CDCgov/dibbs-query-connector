eca03fb68c769e40c0c6fffa0a58b215ea046a2c
fzhao99 <bob@skylight.digital>
2024-10-17 14:32:08 -0400

    align customize query flow to design (#25)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ query-connector/e2e/constants.ts
@@ -0,0 +1,5 @@
+import { hyperUnluckyPatient } from "@/app/constants";
+
+export const TEST_PATIENT = hyperUnluckyPatient;
+export const TEST_PATIENT_NAME =
+  hyperUnluckyPatient.FirstName + " A. " + hyperUnluckyPatient.LastName;
--- query-connector/src/app/query/components/searchForm/SearchForm.tsx
+++ query-connector/src/app/query/components/searchForm/SearchForm.tsx
@@ -10,18 +10,10 @@ import {
   USE_CASES,
   FHIR_SERVERS,
   demoData,
-  PatientType,
-  demoQueryOptions,
-  patientOptions,
   stateOptions,
   Mode,
-  ValueSetItem,
 } from "../../../constants";
-import {
-  UseCaseQueryResponse,
-  UseCaseQuery,
-  UseCaseQueryRequest,
-} from "../../../query-service";
+import { UseCaseQueryResponse, UseCaseQuery } from "../../../query-service";
 import { fhirServers } from "../../../fhir-servers";
 import styles from "./searchForm.module.css";
 
@@ -29,45 +21,41 @@ import { FormatPhoneAsDigits } from "@/app/format-service";
 
 interface SearchFormProps {
   useCase: USE_CASES;
-  queryValueSets: ValueSetItem[];
   setUseCase: (useCase: USE_CASES) => void;
-  setOriginalRequest: (originalRequest: UseCaseQueryRequest) => void;
-  setUseCaseQueryResponse: (UseCaseQueryResponse: UseCaseQueryResponse) => void;
+  setPatientDiscoveryQueryResponse: (
+    UseCaseQueryResponse: UseCaseQueryResponse,
+  ) => void;
   setMode: (mode: Mode) => void;
   setLoading: (loading: boolean) => void;
-  setQueryType: (queryType: string) => void;
+  fhirServer: FHIR_SERVERS;
+  setFhirServer: React.Dispatch<React.SetStateAction<FHIR_SERVERS>>;
 }
 
 /**
  * @param root0 - SearchFormProps
  * @param root0.useCase - The use case this query will cover.
- * @param root0.queryValueSets - Stateful collection of valuesets to use in the query.
  * @param root0.setUseCase - Update stateful use case.
- * @param root0.setOriginalRequest - The function to set the original request.
- * @param root0.setUseCaseQueryResponse - The function to set the use case query response.
  * @param root0.setMode - The function to set the mode.
  * @param root0.setLoading - The function to set the loading state.
- * @param root0.setQueryType - The function to set the query type.
+ * @param root0.setPatientDiscoveryQueryResponse - callback function to set the
+ * patient for use in future steps
+ * @param root0.fhirServer - server to do the query against
+ * @param root0.setFhirServer - callback function to update specified query
  * @returns - The SearchForm component.
  */
 const SearchForm: React.FC<SearchFormProps> = ({
   useCase,
-  queryValueSets,
   setUseCase,
-  setOriginalRequest,
-  setUseCaseQueryResponse,
+  setPatientDiscoveryQueryResponse,
   setMode,
   setLoading,
-  setQueryType,
+  fhirServer,
+  setFhirServer,
 }) => {
   //Set the patient options based on the demoOption
-  const [patientOption, setPatientOption] = useState<string>(
-    patientOptions[useCase]?.[0]?.value || "",
-  );
   const [firstName, setFirstName] = useState<string>("");
   const [lastName, setLastName] = useState<string>("");
-  const [fhirServer, setFhirServer] = useState<FHIR_SERVERS>();
-  // "Public HAPI: Direct",
+
   const [phone, setPhone] = useState<string>("");
   const [dob, setDOB] = useState<string>("");
   const [mrn, setMRN] = useState<string>("");
@@ -75,10 +63,10 @@ const SearchForm: React.FC<SearchFormProps> = ({
   const [showAdvanced, setShowAdvanced] = useState(false);
   const [autofilled, setAutofilled] = useState(false); // boolean indicating if the form was autofilled, changes color if true
 
-  // Fills fields with sample data based on the selected patientOption
+  // Fills fields with sample data based on the selected
   const fillFields = useCallback(
-    (patientOption: PatientType, highlightAutofilled = true) => {
-      const data = demoData[patientOption];
+    (highlightAutofilled = true) => {
+      const data = demoData["cancer"];
       if (data) {
         setFirstName(data.FirstName);
         setLastName(data.LastName);
@@ -90,22 +78,8 @@ const SearchForm: React.FC<SearchFormProps> = ({
         setAutofilled(highlightAutofilled);
       }
     },
-    [patientOption, setUseCase, setQueryType],
+    [setUseCase],
   );
-
-  // Change the selectedDemoOption in the dropdown and update the
-  // query type (which governs the DB fetch) accordingly
-  const handleDemoQueryChange = (selectedDemoOption: string) => {
-    setPatientOption(patientOptions[selectedDemoOption][0].value);
-    setQueryType(
-      demoQueryOptions.find((dqo) => dqo.value == selectedDemoOption)?.label ||
-        "",
-    );
-  };
-
-  const handleClick = () => {
-    setMode("customize-queries");
-  };
 
   async function HandleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
@@ -124,15 +98,10 @@ const SearchForm: React.FC<SearchFormProps> = ({
       use_case: useCase,
       phone: FormatPhoneAsDigits(phone),
     };
-    setOriginalRequest(originalRequest);
-    const queryResponse = await UseCaseQuery(originalRequest, queryValueSets);
-    setUseCaseQueryResponse(queryResponse);
+    const queryResponse = await UseCaseQuery(originalRequest, []);
+    setPatientDiscoveryQueryResponse(queryResponse);
 
-    if (queryResponse.Patient && queryResponse.Patient.length === 1) {
-      setMode("results");
-    } else {
-      setMode("patient-results");
-    }
+    setMode("patient-results");
     setLoading(false);
   }
   useEffect(() => {
@@ -143,7 +112,7 @@ const SearchForm: React.FC<SearchFormProps> = ({
     <>
       <form className="content-container-smaller-width" onSubmit={HandleSubmit}>
         <h1 className="font-sans-2xl text-bold margin-bottom-105">
-          Search for a Patient
+          {STEP_ONE_PAGE_TITLE}
         </h1>
         <h2 className="font-sans-lg text-normal margin-top-0 margin-bottom-105">
           Enter patient information below to search for a patient. We will query
@@ -156,76 +125,22 @@ const SearchForm: React.FC<SearchFormProps> = ({
               htmlFor="query"
             >
               The demo site uses synthetic data to provide examples of possible
-              queries that you can make with the TEFCA Viewer. Select a query
+              queries that you can make with the Query Connector. Select a query
               use case, a sample patient, and then click “fill fields” below.
             </Label>
-            <div className={`${styles.demoQueryDropdownContainer}`}>
-              <div>
-                <Label htmlFor="query">Query</Label>
-                <div className="display-flex flex-align-start query-page-wrapper">
-                  <select
-                    id="query"
-                    name="query"
-                    className="usa-select margin-top-1"
-                    value={useCase}
-                    onChange={(event) => {
-                      handleDemoQueryChange(event.target.value);
-                      setUseCase(event.target.value as USE_CASES);
-                    }}
-                  >
-                    <option value="" disabled>
-                      {" "}
-                      -- Select an Option --{" "}
-                    </option>
-                    {demoQueryOptions.map((option) => (
-                      <option key={option.value} value={option.value}>
-                        {option.label}
-                      </option>
-                    ))}
-                  </select>
-                </div>
-              </div>
-              <div>
-                <Label htmlFor="patient">Patient</Label>
-                <div className="display-flex flex-align-start query-page-wrapper">
-                  <select
-                    id="patient"
-                    name="patient"
-                    className="usa-select margin-top-1"
-                    value={patientOption}
-                    onChange={(event) => {
-                      setPatientOption(event.target.value);
-                    }}
-                  >
-                    {patientOptions[useCase]?.map((option) => (
-                      <option key={option.value} value={option.value}>
-                        {option.label}
-                      </option>
-                    ))}
-                  </select>
-                </div>
-              </div>
-            </div>
 
             <div className={`${styles.searchCallToActionContainer}`}>
               <Button
-                className={`"usa-button" ${styles.searchCallToActionButton}`}
+                className={`usa-button usa-button--outline bg-white`}
                 type="button"
                 onClick={() => {
-                  fillFields(patientOption as PatientType, false);
+                  fillFields(false);
                 }}
               >
                 Fill fields
               </Button>
               <Button
-                type="button"
-                className={`usa-button--outline bg-white ${styles.searchCallToActionButton}`}
-                onClick={() => handleClick()}
-              >
-                Customize query
-              </Button>
-              <Button
-                className={`usa-button--unstyled margin-left-auto ${styles.searchCallToActionButton}`}
+                className={`usa-button--unstyled margin-left-auto`}
                 type="button"
                 onClick={() => {
                   setShowAdvanced(!showAdvanced);
@@ -236,29 +151,29 @@ const SearchForm: React.FC<SearchFormProps> = ({
             </div>
           </div>
         }
-        <Fieldset className={`${styles.searchFormContainer}`}>
+        <Fieldset className={`${styles.searchFormContainer} bg-white`}>
           {showAdvanced && (
-            <div>
-              <Label htmlFor="fhir_server">
-                <b>FHIR Server (QHIN)</b>
+            <div className="grid-row grid-gap margin-bottom-4">
+              <h3 className={`"font-sans-md" ${styles.searchFormSectionLabel}`}>
+                Advanced
+              </h3>
+              <Label
+                htmlFor="fhir_server"
+                className="no-margin-top-important width-full"
+              >
+                FHIR Server (QHIN)
               </Label>
-              <div className="grid-row grid-gap">
+              <div className="grid-col-6">
                 <div className="usa-combo-box">
                   <Select
                     id="fhir_server"
                     name="fhir_server"
                     value={fhirServer}
-                    defaultValue={""}
-                    // defaultValue={"Public HAPI: Direct"}
                     onChange={(event) => {
                       setFhirServer(event.target.value as FHIR_SERVERS);
                     }}
                     required
                   >
-                    <option value="" disabled>
-                      {" "}
-                      -- Select an Option --{" "}
-                    </option>
                     {Object.keys(fhirServers).map((fhirServer: string) => (
                       <option key={fhirServer} value={fhirServer}>
                         {fhirServer}
@@ -269,12 +184,15 @@ const SearchForm: React.FC<SearchFormProps> = ({
               </div>
             </div>
           )}
-          <h2 className="font-sans-md search-form-section-label">
-            <strong>Name</strong>
-          </h2>
-          <div className="grid-row grid-gap">
+
+          <div className="grid-row grid-gap margin-bottom-4">
+            <h3 className={`"font-sans-md" ${styles.searchFormSectionLabel}`}>
+              Name
+            </h3>
             <div className="tablet:grid-col-6">
-              <Label htmlFor="firstName">First Name</Label>
+              <Label htmlFor="firstName" className="no-margin-top-important">
+                First Name
+              </Label>
               <TextInput
                 id="firstName"
                 name="first_name"
@@ -291,7 +209,9 @@ const SearchForm: React.FC<SearchFormProps> = ({
               />
             </div>
             <div className="tablet:grid-col-6">
-              <Label htmlFor="lastName">Last Name</Label>
+              <Label htmlFor="lastName" className="no-margin-top-important">
+                Last Name
+              </Label>
               <TextInput
                 id="lastName"
                 name="last_name"
@@ -308,12 +228,14 @@ const SearchForm: React.FC<SearchFormProps> = ({
               />
             </div>
           </div>
-          <h2 className="font-sans-md search-form-section-label">
-            <strong>Phone Number</strong>
-          </h2>
-          <div className="grid-row grid-gap">
+          <div className="grid-row grid-gap margin-bottom-4">
+            <h3 className={`"font-sans-md" ${styles.searchFormSectionLabel}`}>
+              Phone Number
+            </h3>
             <div className="grid-col-6">
-              <Label htmlFor="phone">Phone Number</Label>
+              <Label htmlFor="phone" className="no-margin-top-important">
+                Phone Number
+              </Label>
               <TextInput
                 id="phone"
                 name="phone"
@@ -329,38 +251,43 @@ const SearchForm: React.FC<SearchFormProps> = ({
               />
             </div>
           </div>
-          <div>
-            <h2 className="font-sans-md search-form-section-label">
-              <strong>Date of Birth</strong>
-            </h2>
-            <div className="grid-row grid-gap">
-              <div className="grid-col-6">
-                <Label htmlFor="dob">Date of Birth</Label>
-                <div className="usa-date-picker">
-                  <input
-                    className="usa-input"
-                    name="dob"
-                    id="dob"
-                    type="date"
-                    value={dob}
-                    onChange={(event) => {
-                      setDOB(event.target.value);
-                    }}
-                    style={{
-                      backgroundColor:
-                        autofilled && dob ? autofillColor : undefined,
-                    }}
-                  />
-                </div>
+          <div className="grid-row grid-gap margin-bottom-4">
+            <h3 className={`"font-sans-md" ${styles.searchFormSectionLabel}`}>
+              Date of Birth
+            </h3>
+            <div className="grid-col-6">
+              <Label htmlFor="dob" className="no-margin-top-important">
+                Date of Birth
+              </Label>
+              <div className="usa-date-picker">
+                <input
+                  className="usa-input"
+                  name="dob"
+                  id="dob"
+                  type="date"
+                  value={dob}
+                  onChange={(event) => {
+                    setDOB(event.target.value);
+                  }}
+                  style={{
+                    backgroundColor:
+                      autofilled && dob ? autofillColor : undefined,
+                  }}
+                />
               </div>
             </div>
           </div>
-          <h2 className="font-sans-md search-form-section-label">
-            <strong>Address</strong>
-          </h2>
-          <div className="grid-row grid-gap">
+          <div className="grid-row grid-gap margin-bottom-4">
+            <h3 className={`"font-sans-md" ${styles.searchFormSectionLabel}`}>
+              Address
+            </h3>
             <div className="grid-col">
-              <Label htmlFor="street_address_1">Street address</Label>
+              <Label
+                htmlFor="street_address_1"
+                className="no-margin-top-important"
+              >
+                Street address
+              </Label>
               <TextInput
                 id="street_address_1"
                 name="street_address_1"
@@ -368,9 +295,14 @@ const SearchForm: React.FC<SearchFormProps> = ({
               />
             </div>
           </div>
-          <div className="grid-row grid-gap">
+          <div className="grid-row grid-gap margin-bottom-4">
             <div className="grid-col">
-              <Label htmlFor="street_address_2">Address Line 2</Label>
+              <Label
+                htmlFor="street_address_2"
+                className="no-margin-top-important"
+              >
+                Address Line 2
+              </Label>
               <TextInput
                 id="street_address_2"
                 name="street_address_2"
@@ -378,13 +310,17 @@ const SearchForm: React.FC<SearchFormProps> = ({
               />
             </div>
           </div>
-          <div className="grid-row grid-gap">
+          <div className="grid-row grid-gap margin-bottom-4">
             <div className="tablet:grid-col-5">
-              <Label htmlFor="city">City</Label>
+              <Label htmlFor="city" className="no-margin-top-important">
+                City
+              </Label>
               <TextInput id="city" name="city" type="text" />
             </div>
             <div className="tablet:grid-col-3">
-              <Label htmlFor="state">State</Label>
+              <Label htmlFor="state" className="no-margin-top-important">
+                State
+              </Label>
               <Select id="state" name="state" defaultValue="">
                 <option value="" disabled>
                   Select a state
@@ -397,7 +333,9 @@ const SearchForm: React.FC<SearchFormProps> = ({
               </Select>
             </div>
             <div className="tablet:grid-col-4">
-              <Label htmlFor="zip">ZIP code</Label>
+              <Label htmlFor="zip" className="no-margin-top-important">
+                ZIP code
+              </Label>
               <TextInput
                 className="usa-input usa-input--medium"
                 id="zip"
@@ -407,12 +345,14 @@ const SearchForm: React.FC<SearchFormProps> = ({
               />
             </div>
           </div>
-          <h2 className="font-sans-md search-form-section-label">
-            <strong>Medical Record Number (MRN)</strong>
-          </h2>
-          <div className="grid-row grid-gap">
+          <div className="grid-row grid-gap margin-bottom-4">
+            <h3 className={`"font-sans-md" ${styles.searchFormSectionLabel}`}>
+              Medical Record Number (MRN)
+            </h3>
             <div className="grid-col-6">
-              <Label htmlFor="mrn">Medical Record Number</Label>
+              <Label htmlFor="mrn" className="no-margin-top-important">
+                Medical Record Number
+              </Label>
               <TextInput
                 id="mrn"
                 name="mrn"
@@ -440,3 +380,4 @@ const SearchForm: React.FC<SearchFormProps> = ({
 export default SearchForm;
 
 const autofillColor = "#faf3d1";
+export const STEP_ONE_PAGE_TITLE = "Step 1: Enter patient information";
