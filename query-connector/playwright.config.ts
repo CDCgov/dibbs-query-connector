fdea5d620617a76fcb06f445ae9c4ba9b640fba8
bamader <49412165+bamader@users.noreply.github.com>
2024-10-24 17:33:12 -0400

    Add e2e for customize query (#53)

--- query-connector/playwright.config.ts
+++ query-connector/playwright.config.ts
@@ -34,39 +34,20 @@ export default defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
-    },
-
-    {
       name: "firefox",
       use: { ...devices["Desktop Firefox"] },
     },
 
+    /* Test against branded browsers. */
     {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
+      name: "Microsoft Edge",
+      use: { ...devices["Desktop Edge"], channel: "msedge" },
     },
 
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
+    {
+      name: "Google Chrome",
+      use: { ...devices["Desktop Chrome"], channel: "chrome" },
+    },
   ],
 
   /* Hook to ensure DB is started & migrations have run before tests start*/
