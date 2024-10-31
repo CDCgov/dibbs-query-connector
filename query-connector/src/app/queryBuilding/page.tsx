7dc7deda12032987e3c289c3dc9a91cee35ce1e6
Bob Zhao <bob@skylight.digital>
2024-10-31 19:16:46 -0400

    don't export

--- query-connector/src/app/queryBuilding/page.tsx
+++ query-connector/src/app/queryBuilding/page.tsx
@@ -5,7 +5,7 @@ import EmptyQueriesDisplay from "./emptyState/EmptyQueriesDisplay";
  * Component for Query Building Flow
  * @returns The Query Building component flow
  */
-export const QueryBuilding: React.FC = () => {
+const QueryBuilding: React.FC = () => {
   return (
     <div className="main-container">
       <h1 className={styles.queryTitle}>My queries</h1>
