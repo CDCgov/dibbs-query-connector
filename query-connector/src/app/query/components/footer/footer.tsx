3ccf3680e1fe6c9f92ee86be7dc50841cd4acd1d
Katie Campbell Downie <katie.campbelldownie@skylight.digital>
2024-11-01 16:31:27 -0500

    responsive footer styles

--- query-connector/src/app/query/components/footer/footer.tsx
+++ query-connector/src/app/query/components/footer/footer.tsx
@@ -9,30 +9,28 @@ import classNames from "classnames";
 export default function FooterComponent() {
   return (
     <footer className={classNames("usa-footer", styles.footerContainer)}>
-      <div className={styles.cdcLogoContainer}>
-        <div>
+      <div className={styles.cdcLogoContainer}>    
           <Image
             src="/tefca-viewer/CDC_logo.png"
             alt="CDC logo"
             width={62}
             height={36}
             className="usa-footer__logo-img"
-          />
+          />  
+      </div>
+      <div className={styles.cdcFooterTextContainer}>
+        <div>
+            <p className="text-base-lightest">
+              Centers for Disease Control and Prevention
+            </p>
         </div>
         <div>
-          <p className="text-base-lightest">
-            Centers for Disease Control and Prevention
-          </p>
-        </div>
-      </div>
-      <div className={styles.contactUsContainer}>
-        <div className="width-fit-content">
-          <p className="text-base-lightest">
-            For more information about this solution, send us an email at{" "}
-            <a className="text-base-lightest" href="mailto:dibbs@cdc.gov">
-              dibbs@cdc.gov
-            </a>
-          </p>
+            <p className="text-base-lightest">
+              For more information about this solution, send us an email at{" "}
+              <a className="text-base-lightest" href="mailto:dibbs@cdc.gov">
+                dibbs@cdc.gov
+              </a>
+            </p>
         </div>
       </div>
     </footer>
