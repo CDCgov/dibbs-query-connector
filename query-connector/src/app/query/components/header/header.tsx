aa8a43ddd7f5fc7e6cbf663647673566b894bd13
Bob Zhao <bob@skylight.digital>
2024-11-04 09:45:39 -0500

    start abstracting out some color tokens

--- query-connector/src/app/query/components/header/header.tsx
+++ query-connector/src/app/query/components/header/header.tsx
@@ -46,7 +46,7 @@ export default function HeaderComponent() {
             <div className="usa-logo">
               <em className="usa-logo__text text-base-lightest-important">
                 <a
-                  className="font-mono-lg text-white-important"
+                  className="font-mono-lg text-base-lightest-important"
                   href={backLink}
                   title={metadata.title}
                 >
