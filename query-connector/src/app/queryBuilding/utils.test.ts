1577624b17bbba6077865bf81ed5f7fa3e032456
Bob Zhao <bob@skylight.digital>
2024-11-06 12:32:17 -0500

    add test harness

---
+++ query-connector/src/app/queryBuilding/utils.test.ts
@@ -0,0 +1,10 @@
+describe("data util methods for query building", () => {
+  it("mapFetchedDataToFrontendStructure", () => {
+    it("translates backend query to frontend dictionary structure", () => {});
+  });
+
+  it("filterSearchByCategoryAndCondition", () => {
+    it("filters by category (parent level)", () => {});
+    it("filters by condition (child level)", () => {});
+  });
+});
