5db0d6b115092a64e990ec8ee8d6c637cb721484
m-goggins <marcelle.goggins@gmail.com>
2024-11-14 09:47:11 -0800

    forgot to remove ModalRef

--- query-connector/src/app/query/components/header/header.tsx
+++ query-connector/src/app/query/components/header/header.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useRef, useState } from "react";
 import { useRouter, usePathname } from "next/navigation";
-import { Button, Icon, ModalRef } from "@trussworks/react-uswds";
+import { Button, Icon } from "@trussworks/react-uswds";
 import styles from "./header.module.scss";
 import { metadata } from "@/app/constants";
 import classNames from "classnames";
