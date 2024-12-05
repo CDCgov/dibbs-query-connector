32b4596afcdb295c55fbf89380e66946c6189f83
Rob Mitchell <40571882+robertandremitchell@users.noreply.github.com>
2024-12-05 12:04:43 -0500

    Initializing My Queries page frontend when user queries present (#168)
    Co-authored-by: fzhao99 <bob@skylight.digital>

--- query-connector/src/app/queryBuilding/page.tsx
+++ query-connector/src/app/queryBuilding/page.tsx
@@ -1,16 +1,59 @@
 "use client";
-import styles from "./queryBuilding.module.scss";
+import { useContext, useEffect, useState } from "react";
+import UserQueriesDisplay from "./dataState/UserQueriesDisplay";
 import EmptyQueriesDisplay from "./emptyState/EmptyQueriesDisplay";
+import { CustomUserQuery } from "@/app/query-building";
+import { getCustomQueries } from "@/app/database-service";
+import { DataContext } from "@/app/utils";
+import styles from "@/app/queryBuilding/queryBuilding.module.scss";
+
 /**
  * Component for Query Building Flow
  * @returns The Query Building component flow
  */
 const QueryBuilding: React.FC = () => {
+  const context = useContext(DataContext);
+  const [loading, setLoading] = useState(true);
+
+  // Check whether custom queries exist in DB
+  // TODO: We will need to support re-running fetchQueries from DB if/when queries are added/deleted/edited
+  useEffect(() => {
+    if (context?.data === null) {
+      const fetchQueries = async () => {
+        try {
+          const queries = await getCustomQueries();
+          context.setData(queries);
+        } catch (error) {
+          console.error("Failed to fetch queries:", error);
+        } finally {
+          setLoading(false);
+        }
+      };
+      fetchQueries();
+    } else {
+      setLoading(false); // Data already exists, no need to fetch again
+    }
+  }, [context]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  const queries = (context?.data || []) as CustomUserQuery[];
+
   return (
-    <div className="main-container">
-      <h1 className={styles.queryTitle}>My queries</h1>
-      <EmptyQueriesDisplay></EmptyQueriesDisplay>
-    </div>
+    <>
+      {queries.length === 0 ? (
+        <div className="main-container">
+          <h1 className={styles.queryTitle}>My queries</h1>
+          <EmptyQueriesDisplay />
+        </div>
+      ) : (
+        <div className="main-container__wide">
+          <UserQueriesDisplay queries={queries} />
+        </div>
+      )}
+    </>
   );
 };
 
