fbaf49993e5ec5cf843e399ae361cc9797424c07
shanice-skylight <shanice@skylight.digital>
2025-01-07 15:18:14 -0500

    added next.config.js back and modified its basePath to fix webpack error

---
+++ query-connector/next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const path = require("path");
+
+const nextConfig = {
+  sassOptions: {
+    includePaths: [
+      path.join(__dirname, "./", "node_modules", "@uswds", "uswds", "packages"),
+    ],
+  },
+  transpilePackages: ["yaml"],
+
+  output: "standalone",
+  basePath: "/",
+};
+
+module.exports = nextConfig;
