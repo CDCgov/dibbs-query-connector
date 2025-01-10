66af5725fa6e55654bbab1707e5c2173dc5007ee
Katie Campbell Downie <katie.campbelldownie@skylight.digital>
2025-01-10 17:49:39 -0600

    merge conflict cleanup

--- query-connector/src/app/queryBuilding/components/SelectionViewAccordionHeader.tsx
+++ query-connector/src/app/queryBuilding/components/SelectionViewAccordionHeader.tsx
@@ -1,13 +1,12 @@
 import styles from "../buildFromTemplates/conditionTemplateSelection.module.scss";
 import { Icon } from "@trussworks/react-uswds";
 import { DibbsConceptType, DibbsValueSet } from "@/app/constants";
-import { tallyConceptsForValueSetArray } from "../utils";
 import { ChangeEvent } from "react";
 import Checkbox from "@/app/query/designSystem/checkbox/Checkbox";
 
 type ConceptTypeAccordionBodyProps = {
-  activeValueSetType: DibbsConceptType;
-  activeValueSets: { [vsId: string]: DibbsValueSet };
+  activeType: DibbsConceptType;
+  activeTypeValueSets: { [vsId: string]: DibbsValueSet };
   expanded: boolean;
   handleVsNameLevelUpdate: (
     vsId: string,
@@ -17,42 +16,28 @@ type ConceptTypeAccordionBodyProps = {
 /**
  * Fragment component to style out some of the accordion bodies
  * @param param0 - params
- * @param param0.activeValueSetType - DibbsactiveValueSetType (labs, conditions, medications)
- * @param param0.activeValueSets - ValueSets for a given activeValueSetType
+ * @param param0.activeType - DibbsactiveValueSetType (labs, conditions, medications)
+ * @param param0.activeTypeValueSets - ValueSets for a given activeValueSetType
  * @param param0.expanded - Boolean for managing icon orientation
- * @param param0.handleVsIdLevelUpdate - curried state update function that
+ * @param param0.handleVsNameLevelUpdate - curried state update function that
  * takes a VsName and generatesa ValueSet level update
  * @returns An accordion body component
  */
-
-/**
- *
- * @param root0
- * @param root0.activeValueSetType
- * @param root0.activeValueSets
- * @param root0.expanded
- * @param root0.handleVsNameLevelUpdate
- */
 const ConceptTypeAccordionHeader: React.FC<ConceptTypeAccordionBodyProps> = ({
-  activeValueSetType,
-  activeValueSets,
+  activeType,
+  activeTypeValueSets,
   expanded,
   handleVsNameLevelUpdate,
 }) => {
-  const selectedCount = tallyConceptsForValueSetArray(
-    Object.values(activeTypeValueSets),
-    true,
-  );
-  const totalCount = tallyConceptsForValueSetArray(
-    Object.values(activeTypeValueSets),
-    false,
-  );
+  const concepts = activeTypeValueSets[activeType].concepts;
+  const selectedCount = concepts.filter((c) => c.include).length;
+  const totalCount = concepts.length;
 
   function handleBulkToggle(
     e: ChangeEvent<HTMLInputElement>,
     isMinusState: boolean,
   ) {
-    Object.entries(activeValueSets).forEach(([vsId, activeValueSets]) => {
+    Object.entries(activeTypeValueSets).forEach(([vsId, activeValueSets]) => {
       const handleValueSetUpdate = handleVsNameLevelUpdate(vsId);
       const bulkIncludeValue = isMinusState ? false : e.target.checked;
       activeValueSets.includeValueSet = bulkIncludeValue;
