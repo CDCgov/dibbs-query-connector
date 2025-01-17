b990db5e35de0a14aaac694d0b01c60d03469c2f
m-goggins <marcelle.goggins@gmail.com>
2025-01-17 14:10:47 -0800

    genericize how we check for included elements in a resource

--- query-connector/src/app/query/components/resultsView/tableComponents/utils.ts
+++ query-connector/src/app/query/components/resultsView/tableComponents/utils.ts
@@ -10,18 +10,32 @@ function isLengthwise(thing: unknown): thing is Lengthwise {
  * Helper function to not display tables in results view where there are no
  * elements in a column to display. Allows for non-lengthwise properties
  * @param array - an array of items to display in a table
- * @param propertyToCheck - the property that we want to conditionally display
+ * @param propertiesToCheck - the property that we want to conditionally display
  * in a table column if some element exists
  * @returns true or false for whether there is any element that exists for the
  * column to be rendered
  */
-export function checkIfSomeElementWithPropertyExists<T, K extends keyof T>(
-  array: T[],
-  propertyToCheck: K
-): boolean {
-  return array
-    .map((e) => e[propertyToCheck])
-    .some((prop) => {
-      return prop !== undefined && (!isLengthwise(prop) || prop.length > 0);
-    });
+export function checkIfSomeElementWithPropertyExists<
+  T extends object,
+  K extends keyof T,
+>(array: T[], propertiesToCheck: K[]): Record<K, boolean> {
+  const result: Record<K, boolean> = propertiesToCheck.reduce(
+    (acc, p) => {
+      acc[p] = false;
+      return acc;
+    },
+    {} as Record<K, boolean>
+  );
+
+  for (const e of array) {
+    for (const p of propertiesToCheck) {
+      const prop = e[p];
+      if ((prop as string) in e) {
+        result[p] = true;
+      }
+    }
+  }
+  console.log(result);
+
+  return result;
 }
