d3f6cbcb2651b53a8693c490e336390fa612f570
bamader <49412165+bamader@users.noreply.github.com>
2025-01-23 14:12:06 -0500

    Make keycloak work in docker compose (#269)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- query-connector/src/auth.ts
+++ query-connector/src/auth.ts
@@ -1,12 +1,25 @@
 import NextAuth from "next-auth";
-import Keycloak from "next-auth/providers/keycloak";
+import KeycloakProvider from "next-auth/providers/keycloak";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  secret: process.env.AUTH_SECRET,
+  trustHost: true,
+  basePath: "/api/auth",
   providers: [
-    Keycloak({
+    KeycloakProvider({
+      jwks_endpoint: `${process.env.NAMED_KEYCLOAK}/realms/master/protocol/openid-connect/certs`,
+      wellKnown: undefined,
       clientId: process.env.AUTH_KEYCLOAK_ID,
       clientSecret: process.env.AUTH_KEYCLOAK_SECRET,
-      issuer: process.env.AUTH_KEYCLOAK_ISSUER,
+      issuer: `${process.env.LOCAL_KEYCLOAK}/realms/master`,
+      authorization: {
+        params: {
+          scope: "openid email profile",
+        },
+        url: `${process.env.LOCAL_KEYCLOAK}/realms/master/protocol/openid-connect/auth`,
+      },
+      token: `${process.env.NAMED_KEYCLOAK}/realms/master/protocol/openid-connect/token`,
+      userinfo: `${process.env.NAMED_KEYCLOAK}/realms/master/protocol/openid-connect/userinfo`,
     }),
   ],
   callbacks: {
