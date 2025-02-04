05dbe6954971fe96d5bd1f38978c2e0d43eac0e2
Katie Campbell Downie <katie.campbelldownie@skylight.digital>
2025-02-04 15:55:28 -0600

    revert Dockerfile changes, update path in db-creation

--- query-connector/src/app/backend/dbCreation/db-creation.ts
+++ query-connector/src/app/backend/dbCreation/db-creation.ts
@@ -309,7 +309,7 @@ function readJsonFromRelativePath(filename: string) {
   try {
     // Re-scope file system reads to make sure we use the relative
     // path via node directory resolution
-    const runtimeServerPath = path.join("/app/assets", filename);
+    const runtimeServerPath = path.join(__dirname, "../../assets/", filename);
     const data = fs.readFileSync(runtimeServerPath, "utf-8");
     return data;
   } catch (error) {
