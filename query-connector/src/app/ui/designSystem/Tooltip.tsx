09aee59cdf3c6ced995b06b72b095bd588654732
fzhao99 <bob@skylight.digital>
2025-02-05 14:14:22 -0500

    valueset search part one (#341)

---
+++ query-connector/src/app/ui/designSystem/Tooltip.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+// https://trussworks.github.io/react-uswds/?path=/docs/components-tooltip--docs#custom%20component
+
+export type TooltipWrapperProps = {
+  className?: string;
+} & JSX.IntrinsicElements["div"] &
+  React.RefAttributes<HTMLDivElement>;
+
+const DivForwardRef: React.ForwardRefRenderFunction<
+  HTMLDivElement,
+  TooltipWrapperProps
+> = ({ className, children, ...tooltipProps }: TooltipWrapperProps, ref) => (
+  <div ref={ref} className={className} {...tooltipProps}>
+    {children}
+  </div>
+);
+const TooltipWrapper = React.forwardRef(DivForwardRef);
+
+export default TooltipWrapper;
