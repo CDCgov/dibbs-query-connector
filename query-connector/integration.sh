9b94730172cd84680311f885884bfb2b83b8306b
Bob Zhao <bob@skylight.digital>
2025-02-06 12:40:55 -0500

    wip

--- query-connector/integration.sh
+++ query-connector/integration.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 set -e  # Exit immediately if a command exits with a non-zero status
-
+docker compose down --volumes --remove-orphans
 docker compose -f docker-compose-integration.yaml up -d
 
 # wait for flyway to finish running before...
 docker compose -f docker-compose-integration.yaml logs -f flyway | grep -q "Successfully applied\|No migration necessary"
 
 # running our integration tests
-DATABASE_URL=postgresql://postgres:pw@localhost:5432/tefca_db TEST_TYPE=integration jest --testPathPattern=tests/integration
+DATABASE_URL=postgresql://postgres:pw@localhost:5432/tefca_db TEST_TYPE=integration jest --testPathPattern=tests/integration --ci --json --coverage --testLocationInResults --outputFile=/coverage/integration/report.json
 JEST_EXIT_CODE=$?
 
 # Teardown containers
