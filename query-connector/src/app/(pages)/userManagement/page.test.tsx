d73c7e9d5a4a050316adb41c6ba70c9e0e7edd3d
johanna-skylight <johanna@skylight.digital>
2025-02-18 15:45:59 -0500

    Implemented unit testing for the user tab

--- query-connector/src/app/(pages)/userManagement/page.test.tsx
+++ query-connector/src/app/(pages)/userManagement/page.test.tsx
@@ -1,8 +1,9 @@
-import UserManagement from "./page";
+import { ToastContainer } from "react-toastify";
 import { render, waitFor, screen } from "@testing-library/react";
 import * as UserManagementBackend from "@/app/backend/user-management";
 import { RoleTypeValues } from "@/app/models/entities/user-management";
-import { RootProviderMock } from "@/app/tests/unit/setup";
+import { RootProviderMock, renderWithUser } from "@/app/tests/unit/setup";
+import UserManagement from "./page";
 
 jest.mock("@/app/backend/user-management", () => ({
   getUsers: jest.fn().mockResolvedValue({ items: [], totalItems: 0 }),
@@ -51,7 +52,7 @@ describe("User Management: User tab", () => {
           ],
         },
       ],
-      totalItems: 1,
+      totalItems: 2,
     });
 
     render(
@@ -76,6 +77,7 @@ describe("User Management: User tab", () => {
     render(
       <RootProviderMock currentPage="/userManagement">
         <UserManagement />
+        <ToastContainer />
       </RootProviderMock>,
     );
 
@@ -84,6 +86,53 @@ describe("User Management: User tab", () => {
     });
 
     expect(screen.getByText("Loading...")).toBeInTheDocument();
-    // how do I check the toast here?
+    expect(screen.getByRole("alert")).toHaveTextContent(
+      "Unable to retrieve users. Please try again.",
+    );
+  });
+
+  it("Changes a user's role", async () => {
+    const userMock = {
+      id: "123",
+      username: "harrypotter",
+      first_name: "harry",
+      last_name: "potter",
+      qc_role: RoleTypeValues.Admin,
+    };
+
+    jest.spyOn(UserManagementBackend, "getUsers").mockResolvedValue({
+      items: [userMock],
+      totalItems: 1,
+    });
+
+    const updateRoleFnSpy = jest
+      .spyOn(UserManagementBackend, "updateUserRole")
+      .mockResolvedValue({
+        totalItems: 1,
+        items: [
+          {
+            ...userMock,
+            qc_role: RoleTypeValues.SuperAdmin,
+          },
+        ],
+      });
+
+    const { user } = renderWithUser(
+      <RootProviderMock currentPage="/userManagement">
+        <UserManagement />
+      </RootProviderMock>,
+    );
+
+    await waitFor(() => {
+      expect(screen.queryByText("Loading")).not.toBeInTheDocument();
+    });
+
+    const dropdownEl = screen.getByRole("combobox");
+    expect(dropdownEl).toHaveValue("Admin");
+
+    await user.selectOptions(dropdownEl, ["Super Admin"]);
+
+    expect(dropdownEl).toHaveValue("Super Admin");
+    expect(updateRoleFnSpy).toHaveBeenCalledWith(userMock.id, "Super Admin");
   });
 });
