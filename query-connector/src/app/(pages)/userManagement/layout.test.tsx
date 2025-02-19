2e65f9feb1f87e70c23df26e5de3ecc820ef5da2
johanna-skylight <johanna@skylight.digital>
2025-02-19 09:56:46 -0500

    Added parameterized tests in layout

--- query-connector/src/app/(pages)/userManagement/layout.test.tsx
+++ query-connector/src/app/(pages)/userManagement/layout.test.tsx
@@ -2,7 +2,10 @@ import { useContext } from "react";
 import { waitFor, screen } from "@testing-library/react";
 import { renderWithUser, RootProviderMock } from "@/app/tests/unit/setup";
 import UserManagementLayout from "./layout";
-import { UserManagementContext } from "./components/UserManagementProvider";
+import {
+  SubjectType,
+  UserManagementContext,
+} from "./components/UserManagementProvider";
 
 jest.mock(
   "@/app/ui/components/withAuth/WithAuth",
@@ -10,48 +13,52 @@ jest.mock(
     ({ children }: React.PropsWithChildren) => <div>{children}</div>,
 );
 
+type MockComponentProps = { type: SubjectType };
+
+const MockComponent: React.FC<MockComponentProps> = ({ type }) => {
+  const { openEditSection } = useContext(UserManagementContext);
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          openEditSection("Mock title", "Mock subtitle", type, "123");
+        }}
+      >
+        Open edit section
+      </button>
+    </div>
+  );
+};
+
 describe("User Management Layout", () => {
-  const MockComponent: React.FC = () => {
-    const { openEditSection } = useContext(UserManagementContext);
+  it.each(["Query", "Members"] as SubjectType[])(
+    "Integrates the '%s' edit section with all inner pages ",
+    async (sectionType: SubjectType) => {
+      const { user } = renderWithUser(
+        <RootProviderMock currentPage="/userManagement">
+          <UserManagementLayout>
+            <MockComponent type={sectionType} />
+          </UserManagementLayout>
+        </RootProviderMock>,
+      );
 
-    return (
-      <div>
-        <button
-          onClick={() => {
-            openEditSection("Mock title", "Mock subtitle", "Query", "123");
-          }}
-        >
-          Open edit section
-        </button>
-      </div>
-    );
-  };
+      await waitFor(() => {
+        expect(screen.queryByText("Loading")).not.toBeInTheDocument();
+      });
 
-  it("Integrates the query edit section with all inner pages ", async () => {
-    const { user } = renderWithUser(
-      <RootProviderMock currentPage="/userManagement">
-        <UserManagementLayout>
-          {" "}
-          <MockComponent />
-        </UserManagementLayout>
-      </RootProviderMock>,
-    );
-
-    await waitFor(() => {
-      expect(screen.queryByText("Loading")).not.toBeInTheDocument();
-    });
-
-    const openBtn = screen.getByRole("button", { name: "Open edit section" });
-    expect(screen.queryByText("Mock title")).not.toBeInTheDocument();
-    // Open section
-    await user.click(openBtn);
-    expect(screen.queryByText("Mock title")).toBeInTheDocument();
-    expect(screen.queryByTestId("drawer-open-true")).toBeInTheDocument();
-    expect(document.body).toMatchSnapshot();
-    // Close section
-    const closeBtn = screen.getByTestId("close-drawer");
-    await user.click(closeBtn);
-    expect(screen.queryByTestId("drawer-open-true")).not.toBeInTheDocument();
-    expect(screen.queryByTestId("drawer-open-false")).toBeInTheDocument();
-  });
+      const openBtn = screen.getByRole("button", { name: "Open edit section" });
+      expect(screen.queryByText("Mock title")).not.toBeInTheDocument();
+      // Open section
+      await user.click(openBtn);
+      expect(screen.queryByText("Mock title")).toBeInTheDocument();
+      expect(screen.queryByTestId("drawer-open-true")).toBeInTheDocument();
+      expect(document.body).toMatchSnapshot();
+      // Close section
+      const closeBtn = screen.getByTestId("close-drawer");
+      await user.click(closeBtn);
+      expect(screen.queryByTestId("drawer-open-true")).not.toBeInTheDocument();
+      expect(screen.queryByTestId("drawer-open-false")).toBeInTheDocument();
+    },
+  );
 });
