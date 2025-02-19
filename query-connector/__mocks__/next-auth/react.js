7d8c13b3b7211abcd94e6f1d463391ee8534fd9b
johanna-skylight <johanna@skylight.digital>
2025-02-19 12:42:54 -0500

    Removed comment

--- query-connector/__mocks__/next-auth/react.js
+++ query-connector/__mocks__/next-auth/react.js
@@ -1,6 +1,7 @@
 /* eslint-disable jsdoc/require-returns, jsdoc/require-param-description */
 // __mocks__/next-auth/react.js
 const useSession = jest.fn().mockReturnValue({ data: undefined });
+
 /**
  *
  * @param root0
