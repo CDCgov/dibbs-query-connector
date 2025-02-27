a41b1557c71176e4981fb81afdef3842f15159f8
fzhao99 <bob@skylight.digital>
2025-02-27 16:01:33 -0500

    add loading skeletons to query execution flow (#395)

--- query-connector/src/app/(pages)/query/components/PatientSearchResults.tsx
+++ query-connector/src/app/(pages)/query/components/PatientSearchResults.tsx
@@ -6,6 +6,7 @@ import Backlink from "../../../ui/designSystem/backLink/Backlink";
 import PatientSearchResultsTable from "./patientSearchResults/PatientSearchResultsTable";
 import NoPatientsFound from "./patientSearchResults/NoPatientsFound";
 import { RETURN_LABEL } from "@/app/(pages)/query/components/stepIndicator/StepIndicator";
+import TitleBox from "./stepIndicator/TitleBox";
 
 /**
  * The props for the PatientSearchResults component.
@@ -15,6 +16,7 @@ export interface PatientSearchResultsProps {
   goBack: () => void;
   setMode: (mode: Mode) => void;
   setPatientForQueryResponse: (patient: Patient) => void;
+  loading: boolean;
 }
 
 /**
@@ -25,6 +27,7 @@ export interface PatientSearchResultsProps {
  * @param root0.setMode - Redirect function to handle results view routing
  * @param root0.setPatientForQueryResponse - Callback function to update the
  * patient being searched for
+ * @param root0.loading - whether the component is in a loading state
  * @returns - The PatientSearchResults component.
  */
 const PatientSearchResults: React.FC<PatientSearchResultsProps> = ({
@@ -32,6 +35,7 @@ const PatientSearchResults: React.FC<PatientSearchResultsProps> = ({
   goBack,
   setPatientForQueryResponse,
   setMode,
+  loading,
 }) => {
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -44,7 +48,21 @@ const PatientSearchResults: React.FC<PatientSearchResultsProps> = ({
 
   return (
     <>
-      {patients.length === 0 && (
+      {(loading || patients.length > 0) && (
+        <>
+          <Backlink onClick={goBack} label={RETURN_LABEL["patient-results"]} />
+
+          <TitleBox step="patient-results" />
+
+          <PatientSearchResultsTable
+            patients={patients}
+            handlePatientSelect={handlePatientSelect}
+            loading={loading}
+          />
+        </>
+      )}
+
+      {!loading && patients.length === 0 && (
         <>
           <NoPatientsFound />
           <a
@@ -54,16 +72,6 @@ const PatientSearchResults: React.FC<PatientSearchResultsProps> = ({
           >
             Revise your patient search
           </a>
-        </>
-      )}
-      {patients.length > 0 && (
-        <>
-          <Backlink onClick={goBack} label={RETURN_LABEL["patient-results"]} />
-
-          <PatientSearchResultsTable
-            patients={patients}
-            handlePatientSelect={handlePatientSelect}
-          />
         </>
       )}
     </>
--- query-connector/src/app/(pages)/query/components/patientSearchResults/PatientSearchResultsTable.tsx
+++ query-connector/src/app/(pages)/query/components/patientSearchResults/PatientSearchResultsTable.tsx
@@ -7,11 +7,12 @@ import {
   formatName,
 } from "@/app/shared/format-service";
 import classNames from "classnames";
-import TitleBox from "../stepIndicator/TitleBox";
+import Skeleton from "react-loading-skeleton";
 
 type PatientSearchResultsTableProps = {
   patients: Patient[];
   handlePatientSelect: (patient: Patient) => void;
+  loading: boolean;
 };
 
 /**
@@ -21,50 +22,62 @@ type PatientSearchResultsTableProps = {
  * @param param0.patients - Patient[] from the FHIR spec to display as rows
  * @param param0.handlePatientSelect - state setter function to redirect
  * to the results view
+ * @param param0.loading -  whether the component is in a loading state
  * @returns The patient search results view
  */
 const PatientSearchResultsTable: React.FC<PatientSearchResultsTableProps> = ({
   patients,
   handlePatientSelect: setPatientForQueryResponse,
+  loading,
 }) => {
   return (
     <>
-      <TitleBox step="patient-results" />
-      <h2 className="page-explainer">The following record(s) match.</h2>
+      <h2 className="page-explainer">
+        {loading ? <Skeleton width={250} /> : "The following record(s) match."}
+      </h2>
+
       <Table className={classNames("margin-top-4")}>
         <thead>
-          <tr>
-            <th>Name</th>
-            <th>DOB</th>
-            <th>Contact</th>
-            <th>Address</th>
-            <th>MRN</th>
-            <th>Actions</th>
-          </tr>
+          {loading ? (
+            <LoadingTableHeader />
+          ) : (
+            <tr>
+              <th>Name</th>
+              <th>DOB</th>
+              <th>Contact</th>
+              <th>Address</th>
+              <th>MRN</th>
+              <th>Actions</th>
+            </tr>
+          )}
         </thead>
         <tbody>
-          {patients.map((patient) => (
-            <tr
-              key={patient.id}
-              className={classNames("tableRowWithHover_clickable")}
-              onClick={() => setPatientForQueryResponse(patient)}
-            >
-              <td>{formatName(patient.name ?? [])}</td>
-              <td width={120}>{patient.birthDate ?? ""}</td>
-              <td>{formatContact(patient.telecom ?? [])}</td>
-              <td>{formatAddress(patient.address ?? [])}</td>
-              <td width={150}>{formatMRN(patient.identifier ?? [])}</td>
-              <td width={100}>
-                <a
-                  href="#"
-                  className="unchanged-color-on-visit"
-                  onClick={() => setPatientForQueryResponse(patient)}
-                >
-                  Select patient
-                </a>
-              </td>
-            </tr>
-          ))}
+          {loading ? (
+            <LoadingTableBody />
+          ) : (
+            patients.map((patient) => (
+              <tr
+                key={patient.id}
+                className={classNames("tableRowWithHover_clickable")}
+                onClick={() => setPatientForQueryResponse(patient)}
+              >
+                <td>{formatName(patient.name ?? [])}</td>
+                <td width={120}>{patient.birthDate ?? ""}</td>
+                <td>{formatContact(patient.telecom ?? [])}</td>
+                <td>{formatAddress(patient.address ?? [])}</td>
+                <td width={150}>{formatMRN(patient.identifier ?? [])}</td>
+                <td width={100}>
+                  <a
+                    href="#"
+                    className="unchanged-color-on-visit"
+                    onClick={() => setPatientForQueryResponse(patient)}
+                  >
+                    Select patient
+                  </a>
+                </td>
+              </tr>
+            ))
+          )}
         </tbody>
       </Table>
     </>
@@ -72,3 +85,31 @@ const PatientSearchResultsTable: React.FC<PatientSearchResultsTableProps> = ({
 };
 
 export default PatientSearchResultsTable;
+
+const LoadingTableHeader: React.FC = () => {
+  return (
+    <tr>
+      {Array.from(Array(7).keys()).map((_) => {
+        return (
+          <th>
+            <Skeleton />
+          </th>
+        );
+      })}
+    </tr>
+  );
+};
+
+const LoadingTableBody: React.FC = () => {
+  return (
+    <tr className={classNames("tableRowWithHover_clickable")}>
+      {Array.from(Array(7).keys()).map((_) => {
+        return (
+          <td>
+            <Skeleton />
+          </td>
+        );
+      })}
+    </tr>
+  );
+};
