fe203bd200a0b04d7b9bb5b351098fe376551b9f
johanna-skylight <johanna@skylight.digital>
2025-03-04 10:27:46 -0500

    Fixed unit testing

--- query-connector/src/app/(pages)/queryBuilding/page.test.tsx
+++ query-connector/src/app/(pages)/queryBuilding/page.test.tsx
@@ -1,20 +1,28 @@
 import QueryBuilding from "./page";
 import { render, screen, waitFor } from "@testing-library/react";
-import {
-  getConditionsData,
-  getCustomQueries,
-} from "../../shared/database-service";
+import { getConditionsData } from "../../shared/database-service";
 import { conditionIdToNameMap, DEFAULT_QUERIES } from "./fixtures";
 import { RootProviderMock } from "@/app/tests/unit/setup";
+import { getQueryList } from "@/app/backend/query-building";
 
 jest.mock(".../../../shared/database-service", () => ({
-  getCustomQueries: jest.fn(),
   getConditionsData: jest.fn(),
 }));
 
+jest.mock("@/app/backend/query-building", () => ({
+  getQueryList: jest.fn(),
+  getCustomQueries: jest.fn(),
+}));
+
+jest.mock(
+  "@/app/ui/components/withAuth/WithAuth",
+  () =>
+    ({ children }: React.PropsWithChildren) => <div>{children}</div>,
+);
+
 describe("tests the query building steps", () => {
   it("renders the empty state", async () => {
-    (getCustomQueries as jest.Mock).mockResolvedValue([]);
+    (getQueryList as jest.Mock).mockResolvedValue([]);
 
     render(
       <RootProviderMock currentPage="/queryBuilding">
@@ -30,13 +38,12 @@ describe("tests the query building steps", () => {
   });
 
   it("renders the default state", async () => {
-    (getCustomQueries as jest.Mock).mockResolvedValue(DEFAULT_QUERIES);
     (getConditionsData as jest.Mock).mockResolvedValue({
       conditionIdToNameMap,
     });
 
     render(
-      <RootProviderMock currentPage="/queryBuilding">
+      <RootProviderMock currentPage="/queryBuilding" data={DEFAULT_QUERIES}>
         <QueryBuilding />
       </RootProviderMock>,
     );
@@ -48,6 +55,7 @@ describe("tests the query building steps", () => {
     });
 
     expect(screen.getByText("Query Library")).toBeInTheDocument();
+
     expectedQueryNames.forEach(async (name) => {
       expect(screen.getByText(name)).toBeInTheDocument();
     });
