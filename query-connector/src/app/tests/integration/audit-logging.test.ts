97c4567deebf77c6f47794807b405ee42180e3ec
Amy Kintner <kintnerae@gmail.com>
2025-03-05 13:46:58 -0800

    update tests

--- query-connector/src/app/tests/integration/audit-logging.test.ts
+++ query-connector/src/app/tests/integration/audit-logging.test.ts
@@ -1,11 +1,26 @@
-import {
-  addUserIfNotExists,
-  updateUserRole,
-  getUsers,
-} from "@/app/backend/user-management";
-import { getDbClient } from "@/app/backend/dbClient";
+import { addUserIfNotExists, updateUserRole } from "@/app/backend/user-management";
 import { auth } from "@/auth";
+import { Bundle, BundleEntry, Patient } from "fhir/r4";
+import { getDbClient } from "@/app/backend/dbClient";
+import { NextRequest } from "next/server";
+import { POST } from "@/app/api/query/route";
+import { readJsonFile } from "../shared_utils/readJsonFile";
 import { RoleTypeValues } from "@/app/models/entities/user-management";
+import { USE_CASE_DETAILS } from "@/app/shared/constants";
+
+// Utility function to create a minimal NextRequest-like object
+function createNextRequest(
+  body: unknown,
+  searchParams: URLSearchParams,
+): NextRequest {
+  return {
+    json: async () => body,
+    text: async () => body,
+    nextUrl: { searchParams },
+    method: "POST",
+    headers: new Headers(),
+  } as unknown as NextRequest;
+}
 
 const dbClient = getDbClient();
 jest.mock("@/auth", () => ({
@@ -25,10 +40,19 @@ const TEST_USER = {
   email: "worf_security@starfleet.com",
   firstName: "Worf",
   lastName: "Mogh",
-  role: 
 };
-
 (auth as jest.Mock).mockResolvedValue(TEST_USER);
+
+const PatientBundle = readJsonFile("./src/app/tests/assets/BundlePatient.json");
+const PatientResource: Patient | undefined = (
+  (PatientBundle as Bundle).entry as BundleEntry[]
+)[0]?.resource as Patient;
+
+if (!PatientResource || PatientResource.resourceType !== "Patient") {
+  throw new Error("Invalid Patient resource in the test bundle.");
+}
+
+const SYPHILIS_QUERY_ID = USE_CASE_DETAILS.syphilis.id;
 
 describe("Audit Logging Integration Tests", () => {
   beforeAll(async () => {
@@ -41,10 +65,34 @@ describe("Audit Logging Integration Tests", () => {
 
   test("should find user and know their role", async () => {
     const result = await addUserIfNotExists(TEST_USER);
+    const createdUserId = result.id;
 
-    expect(result.items![0]).toHaveProperty(
-      "qc_role",
-      RoleTypeValues.Admin,
+    const newresult = await updateUserRole(
+      createdUserId,
+      RoleTypeValues.SuperAdmin,
     );
+    expect(newresult.items![0]).toHaveProperty("qc_role", RoleTypeValues.SuperAdmin);
+  });
+});
+
+describe("Audit Log of POST Query to FHIR Server", () => {
+  beforeEach(() => {
+    // supress the console warns for the error endpoints
+    jest.spyOn(console, "error").mockImplementation(() => {});
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+  it("should create a log if query is successful", async () => {
+    const request = createNextRequest(
+      PatientResource,
+      new URLSearchParams(
+        `id=${SYPHILIS_QUERY_ID}&fhir_server=HELIOS Meld: Direct`,
+      ),
+    );
+    const response = await POST(request);
+    const body = await response.json();
+    expect(body.resourceType).toBe("Bundle");
   });
 });
