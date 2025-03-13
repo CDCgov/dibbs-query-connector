99f44bfb73fdc602900b717ba60e9b21617b8114
Bob Zhao <bob@skylight.digital>
2025-03-13 14:54:23 -0400

    depend on successful completion

--- query-connector/local_e2e.sh
+++ query-connector/local_e2e.sh
@@ -5,7 +5,7 @@ docker compose down --volumes --remove-orphans
 docker compose -f docker-compose-e2e.yaml up -d
 
 # wait for flyway to finish running before...
-docker compose -f docker-compose-e2e.yaml logs -f aidbox_seeder | grep -q "Finished configuring Aidbox and database."
+docker compose -f docker-compose-e2e.yaml logs -f aidbox-seeder | grep -q "Finished configuring Aidbox and database."
 
 npx dotenv -e ./.env -- npx playwright test --ui
 
