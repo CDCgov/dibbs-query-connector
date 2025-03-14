15178296964a3eb6ae4abb70079a27d9e328333a
johanna-skylight <johanna@skylight.digital>
2025-03-14 15:52:37 -0400

    Implemented session timeout modal

--- query-connector/src/app/ui/components/sessionTimeout/sessionTimeout.tsx
+++ query-connector/src/app/ui/components/sessionTimeout/sessionTimeout.tsx
@@ -2,51 +2,65 @@
 
 import { signOut, useSession } from "next-auth/react";
 import { useIdleTimer } from "react-idle-timer";
-import { Modal, ModalRef } from "../../designSystem/modal/Modal";
-import { useRef } from "react";
+import type { ModalProps, ModalRef } from "../../designSystem/modal/Modal";
+import { useEffect, useRef, useState } from "react";
+import dynamic from "next/dynamic";
 
-const IDLE_TIMEOUT = 60;
+const Modal = dynamic<ModalProps>(
+  () => import("../../designSystem/modal/Modal").then((mod) => mod.Modal),
+  { ssr: false },
+);
+
+// 60 mins inactivity
+const IDLE_TIMEOUT_MSEC = 60 * 60000;
+// 5 mins to answer prompt / 5 mins before timeout
+const PROMPT_TIMEOUT_MSEC = 5 * 60000;
 
 /**
  * @returns SessionTimeout component which handles tracking idle time for a logged in user.
  */
 const SessionTimeout: React.FC = () => {
   const { status } = useSession();
+
+  if (status == "unauthenticated") {
+    return null;
+  }
+
+  const [started, setStarted] = useState(false);
   const modalRef = useRef<ModalRef>(null);
-  const { start, reset, getRemainingTime } = useIdleTimer({
-    timeout: IDLE_TIMEOUT * 120000,
-    onIdle,
+  const { activate, start, reset, pause, getRemainingTime } = useIdleTimer({
+    timeout: IDLE_TIMEOUT_MSEC,
+    promptBeforeIdle: PROMPT_TIMEOUT_MSEC,
+    onPrompt: handlePrompt,
+    onIdle: handleLogout,
     stopOnIdle: true,
     startManually: true,
   });
 
-  if (status == "authenticated") {
-    start();
-  }
-
-  async function logout() {
-    await signOut({ redirectTo: "/" });
-  }
-
-  function onIdle() {
-    console.log("on idle");
+  function handlePrompt() {
     modalRef.current?.toggleModal();
+  }
 
-    /* const id = setTimeout(()=>{
-        modalRef.current?.toggleModal();
-        logout().then();
-      },10000);
-*/
+  async function handleLogout() {
+    modalRef.current?.modalIsOpen && modalRef.current?.toggleModal();
+    //stop timer and logout
+    reset();
+    pause();
+    await signOut({ redirect: false }); // TODO tests this again
   }
 
   function handleStay() {
-    console.log("on stay");
-    // clearTimeout(countDown.current);
-    modalRef.current?.toggleModal();
-    reset();
+    modalRef.current?.modalIsOpen && modalRef.current?.toggleModal();
+    activate();
   }
 
-  console.log(getRemainingTime());
+  // Inititalize timer
+  useEffect(() => {
+    if (!started && status === "authenticated") {
+      setStarted(true);
+      start();
+    }
+  }, [status, started, setStarted]);
 
   return (
     <Modal
@@ -67,7 +81,7 @@ const SessionTimeout: React.FC = () => {
           type: "button" as const,
           id: "session-timeout-signout",
           className: "usa-button usa-button--outline",
-          onClick: logout,
+          onClick: handleLogout,
         },
       ]}
     >
--- query-connector/src/app/ui/designSystem/modal/Modal.tsx
+++ query-connector/src/app/ui/designSystem/modal/Modal.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Modal as TrussModal,
   ModalHeading,
