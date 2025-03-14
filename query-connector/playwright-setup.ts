2d70b9b31bcdf575758f145841f4bf64208ade54
Bob Zhao <bob@skylight.digital>
2025-03-14 13:37:59 -0400

    add check for failure

--- query-connector/playwright-setup.ts
+++ query-connector/playwright-setup.ts
@@ -5,6 +5,7 @@ export const TEST_URL = process.env.TEST_ENV ?? "http://localhost:3000/";
 async function globalSetup() {
   const maxRetries = 15; // Maximum number of retries
   const delay = 1000; // Delay between retries in milliseconds
+  let successfulSetup = false;
 
   // Check TEST_URL
   for (let attempts = 0; attempts < maxRetries; attempts++) {
@@ -12,7 +13,8 @@ async function globalSetup() {
       const response = await fetch(TEST_URL);
       if (response.status === 200) {
         console.log(`Connected to ${TEST_URL} successfully.`);
-        break; // Proceed to the FHIR server check
+        successfulSetup = true;
+        break;
       } else {
         console.log(
           `Failed to connect to ${TEST_URL}, status: ${response.status}. Error: ${response.text} Retrying...`,
@@ -29,8 +31,9 @@ async function globalSetup() {
     }
   }
 
-  // we've failed more than the max retries, quit playwright
-  process.exit(1);
+  if (!successfulSetup) {
+    process.exit(1);
+  }
 }
 
 export default globalSetup;
