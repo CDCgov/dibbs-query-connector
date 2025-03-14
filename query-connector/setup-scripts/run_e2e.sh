86238290de5da012461b5d74e4625695c81cdb58
Bob Zhao <bob@skylight.digital>
2025-03-14 11:01:09 -0400

    try adding some organization

--- query-connector/setup-scripts/run_e2e.sh
+++ query-connector/setup-scripts/run_e2e.sh
@@ -4,10 +4,18 @@ set -e  # Exit immediately if a command exits with a non-zero status
 docker compose down --volumes --remove-orphans
 docker compose -f docker-compose-e2e.yaml up -d
 
-# wait for flyway to finish running before...
+# wait for Aidbox seeder to finish running before...
 docker compose -f docker-compose-e2e.yaml logs -f aidbox-seeder | grep -q "Finished configuring Aidbox and database."
 
-npx dotenv -e ./.env -- npx playwright test --ui
+BASE_CMD="npx dotenv -e ./.env -- npx playwright test "
+# running our e2e tests
+if [ "$RUN_WITH_UI" = "true" ]; then 
+    E2E_CMD="$BASE_CMD  --ui"
+else 
+    E2E_CMD="$BASE_CMD --reporter=list"
+fi 
+
+eval $E2E_CMD
 
 # Teardown containers
 docker compose -f docker-compose-integration.yaml down
