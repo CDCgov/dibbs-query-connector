1e51dd6b074deb4f447941db388eb01025ac4fdb
Bob Zhao <bob@skylight.digital>
2025-03-14 11:31:08 -0400

    add log setup

--- query-connector/setup-scripts/e2e_tests.sh
+++ query-connector/setup-scripts/e2e_tests.sh
@@ -7,6 +7,8 @@ docker compose -f docker-compose-e2e.yaml up -d
 # wait for Aidbox seeder to finish running before...
 docker compose -f docker-compose-e2e.yaml logs -f aidbox-seeder | grep -q "Finished configuring Aidbox and database."
 
+docker compose -f docker-compose-e2e.yaml logs > logs-before-tests.txt
+
 BASE_CMD="npx dotenv -e ./.env -- npx playwright test "
 # running our e2e tests
 if [ "$RUN_WITH_UI" = "true" ]; then 
@@ -18,7 +20,9 @@ fi
 eval $E2E_CMD
 E2E_EXIT_CODE=$?
 
+docker compose -f docker-compose-e2e.yaml logs > logs-after-tests.txt
+
 # Teardown containers
-docker compose -f docker-compose-integration.yaml down
+docker compose -f docker-compose-e2e.yaml down
 
 exit $JEST_EXIT_CODE
