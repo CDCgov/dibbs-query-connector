4f04b5db8e5a05edaa71cb59f4e2b31688a2fcc1
Bob Zhao <bob@skylight.digital>
2025-03-14 14:30:20 -0400

    switch

--- query-connector/setup-scripts/e2e_tests.sh
+++ query-connector/setup-scripts/e2e_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker compose down --volumes --remove-orphans
-docker compose -f  docker-compose-e2e.yaml -e AIDBOX_LICENSE=$AIDBOX_LICENSE up  -d
+AIDBOX_LICENSE=$AIDBOX_LICENSE docker compose -f  docker-compose-e2e.yaml up   -d
 
 # wait for Aidbox seeder to finish running before...
 docker compose -f docker-compose-e2e.yaml logs -f aidbox-seeder | grep -q "Finished configuring Aidbox and database."
