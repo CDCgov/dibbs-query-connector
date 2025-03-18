62beec1a8ea179f4b5c5190e88e3d0be085fd983
Bob Zhao <bob@skylight.digital>
2025-03-18 15:39:36 -0400

    remove db hostname

--- query-connector/setup-scripts/e2e_tests.sh
+++ query-connector/setup-scripts/e2e_tests.sh
@@ -5,6 +5,15 @@ docker compose -f docker-compose-e2e.yaml --env-file .env up -d --build
 
 # wait for Aidbox seeder to finish running before...
 docker compose -f docker-compose-e2e.yaml logs -f aidbox-seeder | grep -q "Finished configuring Aidbox and database."
+command_pid=$!
+
+echo -n "Waiting for Aidbox seeder to complete."
+while kill -0 $command_pid 2>/dev/null; do
+    echo -n "."
+    sleep 1
+done
+
+echo -e "\nAidbox seeder finished!"
 
 npx dotenv -e ./.env -- npx playwright test --reporter=list
 E2E_EXIT_CODE=$?
