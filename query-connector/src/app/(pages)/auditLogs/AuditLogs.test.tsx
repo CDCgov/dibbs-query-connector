aa2fc045047a48ecf94530a340391477b42ce23a
Rob Mitchell <robert.a.mitchell@skylight.digital>
2025-03-19 09:20:22 -0400

    fixing audit logs test, punting on datepicker tests in favor of waiting until we implement datepickerange

--- query-connector/src/app/(pages)/auditLogs/AuditLogs.test.tsx
+++ query-connector/src/app/(pages)/auditLogs/AuditLogs.test.tsx
@@ -1,4 +1,10 @@
-import { render, screen, fireEvent, within } from "@testing-library/react";
+import {
+  render,
+  screen,
+  fireEvent,
+  within,
+  waitFor,
+} from "@testing-library/react";
 import AuditLogs from "./page";
 import userEvent from "@testing-library/user-event";
 
@@ -32,77 +38,112 @@ describe("AuditLogs Component", () => {
     render(<AuditLogs />);
   });
 
-  test("renders the audit logs table", () => {
-    expect(screen.getByRole("table")).toBeInTheDocument();
+  test("renders the audit logs table", async () => {
+    await waitFor(() => expect(screen.getByRole("table")).toBeInTheDocument());
   });
 
-  test("filters by name", async () => {
+  test("filters by name and clears filter", async () => {
     await selectDropdownOption("Name(s)", "Rocky Balboa");
 
-    const rows = screen.getAllByRole("row");
-    expect(rows.length).toBeGreaterThan(1); // Ensuring at least header + 1 row
+    const rows = await screen.findAllByRole("row");
+    expect(rows.length).toBeGreaterThan(1);
     rows.slice(1).forEach((row) => {
       expect(within(row).getByText("Rocky Balboa")).toBeInTheDocument();
     });
+
+    await selectDropdownOption("Name(s)", "");
+
+    await waitFor(() => {
+      expect(screen.getAllByRole("row").length).toBeGreaterThan(1);
+    });
   });
 
-  test("filters by action", async () => {
+  test("filters by action and clears filter", async () => {
     await selectDropdownOption("Action(s)", "Created Report");
 
-    const rows = screen.getAllByRole("row");
+    const rows = await screen.findAllByRole("row");
     expect(rows.length).toBeGreaterThan(1);
     rows.slice(1).forEach((row) => {
       expect(within(row).getByText("Created Report")).toBeInTheDocument();
     });
+
+    await selectDropdownOption("Action(s)", "");
+
+    await waitFor(() => {
+      expect(screen.getAllByRole("row").length).toBeGreaterThan(1);
+    });
   });
 
-  // test("filters by date", async () => {
-  //   const dateInput = screen.getByTestId("date-picker-external-input");
-  //   await userEvent.type(dateInput, "03/10/2025");
-  //   const rows = screen.getAllByRole("row");
-  //   expect(rows.length).toBeGreaterThan(1);
-  //   rows.slice(1).forEach((row) => {
-  //     expect(
-  //       within(row).getByText(
-  //         (content) =>
-  //           content.includes("3/10/2025") || content.includes("March 10, 2025"),
-  //       ),
-  //     ).toBeInTheDocument();
-  //   });
-  // });
-
-  test("filters by search field", async () => {
+  test("filters by partial search", async () => {
     await typeInField("Search name or action", "Apollo");
 
-    const rows = screen.getAllByRole("row");
+    const rows = await screen.findAllByRole("row");
     expect(rows.length).toBeGreaterThan(1);
     rows.slice(1).forEach((row) => {
-      expect(within(row).getByText("Apollo Creed")).toBeInTheDocument();
+      expect(within(row).getByText(/Apollo/i)).toBeInTheDocument();
     });
   });
 
   test("pagination updates displayed logs", async () => {
     const nextButton = screen.getByLabelText("Next page");
     expect(nextButton).toBeInTheDocument();
-
     fireEvent.click(nextButton);
 
-    const pageNumbers = screen.getAllByRole("button", { name: /Page/ });
-    expect(pageNumbers[1]).toHaveAttribute("aria-current", "page");
+    await waitFor(() => {
+      const pageNumbers = screen.getAllByRole("button", { name: /Page/ });
+      expect(pageNumbers[1]).toHaveAttribute("aria-current", "page");
+    });
+  });
+
+  test("pagination allows clicking previous only if it exists", async () => {
+    const nextButton = screen.getByLabelText("Next page");
+    expect(nextButton).toBeInTheDocument();
+    fireEvent.click(nextButton);
+
+    // Ensure previous button appears after going to the second page
+    await waitFor(() => {
+      const prevButton = screen.getByLabelText("Previous page");
+      expect(prevButton).toBeInTheDocument();
+      fireEvent.click(prevButton);
+    });
+  });
+
+  test("pagination allows clicking a specific page number", async () => {
+    const pageTwoButton = screen.getByRole("button", { name: "Page 2" });
+
+    fireEvent.click(pageTwoButton);
+
+    await waitFor(() => {
+      expect(pageTwoButton).toHaveAttribute("aria-current", "page");
+    });
   });
 
   test("changing actions per page updates table", async () => {
     await selectDropdownOption("Actions per page", "25");
 
-    const rows = screen.getAllByRole("row");
-    expect(rows.length).toBe(26); // 25 logs + 1 header row
+    await waitFor(() => {
+      const rows = screen.getAllByRole("row");
+      expect(rows.length).toBe(26); // 25 logs + 1 header row
+    });
   });
 
-  test("clearing filters resets results", async () => {
+  test("clear filters resets empty state", async () => {
+    // there is no Apollo Creed with Created Report; will need to come up with a different test
     await selectDropdownOption("Name(s)", "Apollo Creed");
-    await selectDropdownOption("Name(s)", "");
+    await selectDropdownOption("Action(s)", "Created Report");
+    await waitFor(() => {
+      expect(
+        screen.queryByRole("button", { name: "Clear filters" }),
+      ).toBeInTheDocument();
+    });
 
-    const rows = screen.getAllByRole("row");
-    expect(rows.length).toBeGreaterThan(1);
+    const clearFiltersButton = screen.getByRole("button", {
+      name: "Clear filters",
+    });
+    fireEvent.click(clearFiltersButton);
+
+    await waitFor(() => {
+      expect(screen.getByRole("table")).toBeInTheDocument();
+    });
   });
 });
