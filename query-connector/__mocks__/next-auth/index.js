1b8a91cd2edda5263efc4a293e6b84309c84a6d7
Johanna <103958711+johanna-skylight@users.noreply.github.com>
2025-03-26 15:49:35 -0400

    Jd/eslint ignore jest mocks (#466)

--- query-connector/__mocks__/next-auth/index.js
+++ query-connector/__mocks__/next-auth/index.js
@@ -1,5 +1,3 @@
-/* eslint-disable jsdoc/require-returns, jsdoc/require-param-description */
-// __mocks__/next-auth
 const NextAuth = jest.fn().mockReturnValue({
   handlers: undefined,
   signIn: jest.fn(),
