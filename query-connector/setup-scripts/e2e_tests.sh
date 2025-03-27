453d76b34e7fb60698d117109815b4b3985056fa
Bob Zhao <bob@skylight.digital>
2025-03-27 10:49:40 -0400

    add logs

--- query-connector/setup-scripts/e2e_tests.sh
+++ query-connector/setup-scripts/e2e_tests.sh
@@ -7,6 +7,8 @@ docker compose -f docker-compose-e2e.yaml --env-file .env up -d --build
 docker compose -f docker-compose-e2e.yaml logs -f aidbox-seeder | grep -q "Finished configuring Aidbox and database."
 command_pid=$!
 
+docker compose -f docker-compose-e2e.yaml logs aidbox-seeder > logs-before-tests.txt
+
 echo -n "Waiting for Aidbox seeder to complete."
 while kill -0 $command_pid 2>/dev/null; do
     echo -n "."
@@ -17,6 +19,8 @@ echo -e "\nAidbox seeder finished!"
 
 npx dotenv -e ./.env -- npx playwright test --reporter=list
 E2E_EXIT_CODE=$?
+
+docker compose -f docker-compose-e2e.yaml aidbox-seeder logs > logs-after-tests.txt
 
 # Teardown containers
 docker compose -f docker-compose-e2e.yaml down
