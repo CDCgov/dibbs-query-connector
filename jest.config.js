607c2418d06e83fa4f1f2dca934a88b76b04d229
Nick Clyde <nick@clyde.tech>
2025-05-09 10:08:29 -0700

    Try just the one file

--- jest.config.js
+++ jest.config.js
@@ -18,7 +18,9 @@ const customJestConfig = {
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
   },
-  transformIgnorePatterns: ["/node_modules/(?!next-auth)"],
+  transformIgnorePatterns: [
+    "/node_modules/next-auth/providers/microsoft-entra-id.js",
+  ],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
