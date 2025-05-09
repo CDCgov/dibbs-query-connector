722a727e872dccabe9791c3b44143fe1c3f6d743
Nick Clyde <nick@clyde.tech>
2025-05-09 10:20:15 -0700

    Gotta do a hack

--- jest.config.js
+++ jest.config.js
@@ -18,16 +18,19 @@ const customJestConfig = {
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
   },
-  transformIgnorePatterns: [
-    "/node_modules/next-auth/providers/microsoft-entra-id.js",
-  ],
 };
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+// work around https://github.com/vercel/next.js/issues/35634
 /**
- * Creates and exports Jest configuration.
- * @returns The Jest configuration object.
+ * This function is a workaround for the issue with Next.js and Jest
+ * @returns - The Jest config object
  */
-module.exports = async () => ({
-  ...(await createJestConfig(customJestConfig)()),
-});
+async function hackJestConfig() {
+  // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+  const nextJestConfig = await createJestConfig(customJestConfig)();
+  // /node_modules/ is the first pattern, so overwrite it with the correct version
+  nextJestConfig.transformIgnorePatterns[0] = "/node_modules/(?!(next-auth))/";
+  return nextJestConfig;
+}
+
+export default hackJestConfig;
