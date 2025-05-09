bb3018f4378907204f51250c47d2590297be9dd4
Nick Clyde <nick@clyde.tech>
2025-05-09 10:42:01 -0700

    Try pattern again

--- jest.config.js
+++ jest.config.js
@@ -29,8 +29,7 @@ async function hackJestConfig() {
   // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
   const nextJestConfig = await createJestConfig(customJestConfig)();
   // /node_modules/ is the first pattern, so overwrite it with the correct version
-  nextJestConfig.transformIgnorePatterns[0] =
-    "/node_modules/next-auth/providers/microsoft-entra-id.js";
+  nextJestConfig.transformIgnorePatterns[0] = "/node_modules/(?!next-auth)/";
   return nextJestConfig;
 }
 
