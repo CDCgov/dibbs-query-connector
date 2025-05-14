5209f109da43b8a944d3e14f6a50c08bf277b612
Katie Campbell Downie <katie.campbelldownie@skylight.digital>
2025-05-14 12:20:51 -0500

    add some more unit tests

--- src/app/(pages)/codeLibrary/CodeLibrary.test.tsx
+++ src/app/(pages)/codeLibrary/CodeLibrary.test.tsx
@@ -1,4 +1,4 @@
-import { screen, render, act } from "@testing-library/react";
+import { screen, render, act, waitFor } from "@testing-library/react";
 import { RootProviderMock } from "@/app/tests/unit/setup";
 import {
   gonorrheaValueSets,
@@ -13,7 +13,10 @@ import {
   getConditionsData,
 } from "@/app/shared/database-service";
 import { getAllUsers, getUserByUsername } from "@/app/backend/user-management";
-import { getAllGroupMembers } from "@/app/backend/usergroup-management";
+import {
+  getAllGroupMembers,
+  getAllUserGroups,
+} from "@/app/backend/usergroup-management";
 import { mockAdmin } from "../userManagement/test-utils";
 import { getCustomValueSetById } from "@/app/shared/custom-code-service";
 
@@ -32,6 +35,7 @@ jest.mock(".../../../shared/database-service", () => ({
 }));
 
 jest.mock("@/app/backend/usergroup-management", () => ({
+  getAllUserGroups: jest.fn(),
   getAllGroupMembers: jest.fn(),
 }));
 
@@ -116,6 +120,9 @@ describe("Code library rendered view", () => {
     expect(tableBody.childNodes[1]).toHaveTextContent(
       gonorrheaValueSets[1].valueset_name,
     );
+    expect(tableBody.childNodes[2]).toHaveTextContent(
+      customValueSets[1].valueset_name,
+    );
     expect(document.body).toMatchSnapshot();
   });
 
@@ -140,5 +147,113 @@ describe("Code library rendered view", () => {
 
     expect(tableBody.childNodes).toHaveLength(3);
     expect(document.body).toMatchSnapshot();
+  });
+});
+
+describe("Code library interaction", () => {
+  const valueSets = [
+    cancerValueSets,
+    gonorrheaValueSets,
+    customValueSets,
+  ].flat();
+  beforeAll(() => {
+    (getAllUsers as jest.Mock).mockResolvedValue({ items: [], totalItems: 0 });
+    (getAllUserGroups as jest.Mock).mockResolvedValue({
+      items: [],
+      totalItems: 1,
+    });
+
+    (getAllGroupMembers as jest.Mock).mockResolvedValue({
+      items: [],
+      totalItems: 0,
+    });
+    (getUserByUsername as jest.Mock).mockResolvedValue({ items: [mockAdmin] });
+
+    (getConditionsData as jest.Mock).mockResolvedValue({
+      conditionIdToNameMap,
+      categoryToConditionNameArrayMap,
+    });
+
+    (getAllValueSets as jest.Mock).mockReturnValue({ items: valueSets });
+
+    (getCustomValueSetById as jest.Mock).mockReturnValue({
+      items: customValueSets[0],
+    });
+  });
+  it("renders action buttons for custom value sets only", async () => {
+    await act(async () =>
+      render(
+        <RootProviderMock currentPage="/codeLibrary">
+          <CodeLibrary />
+        </RootProviderMock>,
+      ),
+    );
+    expect(screen.getByTestId("table-valuesets")).toBeInTheDocument();
+
+    const tableBody = screen.getByTestId("table-valuesets");
+    const cancerVsRow = tableBody.getElementsByClassName(
+      "valueSetTable__tableBody_row_details",
+    )[0];
+    const customVsRow = tableBody.getElementsByClassName(
+      "valueSetTable__tableBody_row_details",
+    )[2];
+
+    expect(cancerVsRow.textContent).toInclude(cancerValueSets[0].valueset_name);
+    expect(customVsRow.textContent).toInclude(customValueSets[0].valueset_name);
+
+    expect(cancerVsRow.textContent).not.toInclude(cancerValueSets[0].author);
+    expect(customVsRow.textContent).toInclude(customValueSets[0].author);
+
+    await waitFor(async () => {
+      (cancerVsRow as HTMLElement).click();
+      const cancerCodesPanel = screen.getByTestId("table-codes").parentElement;
+      expect(cancerCodesPanel).toHaveTextContent(
+        "This value set comes from DIBBs and cannot be modified.",
+      );
+    });
+
+    await waitFor(async () => {
+      (customVsRow as HTMLElement).click();
+      const customCodesPanel = screen.getByTestId("table-codes").parentElement;
+
+      const customActionButtons = (
+        customCodesPanel as Element
+      ).getElementsByClassName("usa-button");
+      expect(customActionButtons[0]).toHaveTextContent("Edit codes");
+      expect(customActionButtons[1]).toHaveTextContent("Delete value set");
+    });
+
+    expect(document.body).toMatchSnapshot();
+  });
+
+  it("prompts the user to confirm before deleting a value set", async () => {
+    await act(async () =>
+      render(
+        <RootProviderMock currentPage="/codeLibrary">
+          <CodeLibrary />
+        </RootProviderMock>,
+      ),
+    );
+
+    const customVsRow = screen
+      .getByTestId("table-valuesets")
+      .getElementsByClassName("valueSetTable__tableBody_row_details")[2];
+
+    await waitFor(async () => {
+      (customVsRow as HTMLElement).click();
+      const customCodesPanel = screen.getByTestId("table-codes").parentElement;
+
+      const deleteButton = (customCodesPanel as Element).getElementsByClassName(
+        "usa-button",
+      )[1];
+
+      (deleteButton as HTMLElement).click();
+
+      const modal = screen.getByTestId("modalWindow");
+      expect(modal).toBeVisible();
+      expect(modal).toHaveTextContent(
+        `Are you sure you want to delete the value set "${customValueSets[0].valueset_name}?"`,
+      );
+    });
   });
 });
