36c91699913132412cdbe2bb23e7de83ce90be7f
Nick Clyde <nick@clyde.tech>
2025-05-22 14:48:12 -0700

    ugh stupid linter

--- src/lib/remark-image-transform.mjs
+++ src/lib/remark-image-transform.mjs
@@ -5,7 +5,7 @@ import { visit } from "unist-util-visit";
  * This plugin modifies the URLs of images in Markdown files to be compatible with Next.js.
  * It specifically looks for image URLs that start with "../../public/"
  * and transforms them to start with "/".
- * @returns {function} A function that takes a Markdown AST and modifies it in place.
+ * @returns A function that takes a Markdown AST and modifies it in place.
  */
 export const remarkImageTransform = () => {
   return (tree) => {
