25a393e69467f58082da96799b28c675300243a2
Bob Zhao <bob@skylight.digital>
2025-05-27 13:09:17 -0400

    add warning suppressions

--- next.config.js
+++ next.config.js
@@ -6,6 +6,9 @@ const nextConfig = {
     includePaths: [
       path.join(__dirname, "./", "node_modules", "@uswds", "uswds", "packages"),
     ],
+    // adding these suppressions until this USWDS fix goes in
+    // https://github.com/uswds/uswds/pull/6418
+    silenceDeprecations: ["global-builtin", "mixed-decls", "legacy-js-api"],
   },
   transpilePackages: ["yaml"],
 
