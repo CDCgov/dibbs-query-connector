10ab960bcc14da74c9cc69ee25b766af6d66fa54
Katie Campbell Downie <katie.campbelldownie@skylight.digital>
2025-06-10 11:25:18 -0500

    ci testing

--- setup-scripts/unit_and_integration_tests.sh
+++ setup-scripts/unit_and_integration_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e  # Exit immediately if a command exits with a non-zero status. Comment this if debugging in CI
+# set -e  # Exit immediately if a command exits with a non-zero status. Comment this if debugging in CI
 docker compose down --volumes --remove-orphans
 docker compose -f docker-compose-integration.yaml up -d
 
@@ -8,8 +8,8 @@ docker compose -f docker-compose-integration.yaml up -d
 docker compose -f docker-compose-integration.yaml logs -f aidbox-seeder | grep -q "Finished configuring Aidbox and database."
 
 # uncomment these and the corresponding block in ci.yaml to get logs in CI. Make sure also to comment the set -e command at the top of this file too!
-# mkdir test-results
-# docker compose -f docker-compose-integration.yaml logs > /test-results/logs-before-tests.txt
+mkdir test-results
+docker compose -f docker-compose-integration.yaml logs > /test-results/logs-before-tests.txt
 
 BASE_CMD="DATABASE_URL=postgresql://postgres:pw@localhost:5432/tefca_db TEST_TYPE=integration npx jest "
 # running our integration tests
