05208570936f029cc79886bc147921cda7a37f72
Bob Zhao <bob@skylight.digital>
2025-06-26 12:06:00 -0400

    kick run

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -14,6 +14,7 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
         style={{ width: "100%", height: "auto" }}
         width={1000}
         height={1000}
+        alt-label={"Image diagram to illustrate documentation concept"}
         {...(props as ImageProps)}
       />
     ),
