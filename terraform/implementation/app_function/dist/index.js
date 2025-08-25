44601d12b9fb11baacb2c3146964deb4cb66e65b
Rob Mitchell <robert.a.mitchell@skylight.digital>
2025-08-25 13:48:11 -0400

    trying to add dist filles

---
+++ terraform/implementation/app_function/dist/index.js
@@ -0,0 +1,2 @@
+import "./src/functions/ProcessHL7/process-hl7.js";
+import "./src/functions/PostHL7/post-hl7.js";
--- terraform/implementation/app_function/dist/src/functions/PostHL7/post-hl7.js
+++ terraform/implementation/app_function/dist/src/functions/PostHL7/post-hl7.js
@@ -1,11 +1,6 @@
-type HL7QueryParams = {
-  endpoint: string;
-  queryId: string;
-  fhirServer: string;
-  serviceToken: string;
-  hl7Message: string;
-};
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.postHL7 = postHL7;
 /**
  * Sends a raw HL7v2 message to the Query Connector API as text/plain.
  * @param root0 - The parameters for the HL7 query.
@@ -16,17 +11,14 @@ type HL7QueryParams = {
  * @param root0.hl7Message - The HL7v2 message to be sent.
  * @returns A promise that resolves to the fetch response.
  */
-export async function postHL7({
+async function postHL7({
   endpoint,
   queryId,
   fhirServer,
   serviceToken,
   hl7Message,
-}: HL7QueryParams): Promise<Response> {
-  const url = `${endpoint}?id=${encodeURIComponent(queryId)}&fhir_server=${encodeURIComponent(
-    fhirServer,
-  )}&message_format=HL7`;
-
+}) {
+  const url = `${endpoint}?id=${encodeURIComponent(queryId)}&fhir_server=${encodeURIComponent(fhirServer)}&message_format=HL7`;
   return fetch(url, {
     method: "POST",
     headers: {
